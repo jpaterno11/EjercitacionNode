aa1b6236a316d28075c02ab89b94c04fc822833b
Android Studio <devandroid@ort>
2025-04-04 11:59:23 -0300

    gracias polshu

--- src/modules/omdb-wrapper.js
+++ src/modules/omdb-wrapper.js
@@ -1,39 +1,41 @@
 import axios from "axios";
 const APIKEY = "88c05cde";
 const OMDBSearchByPage = async (searchText, page = 1) => {
-let returnObject = {
-respuesta : false,
-datos : []
+    let returnObject = {
+    respuesta : false,
+    datos : []
+    };
+        const response = await axios.get(`http://www.omdbapi.com/?s=${searchText}&page=${page}&apikey=${APIKEY}`);
+        if (response.data.Response === "True") {
+            returnObject.respuesta = true;
+            returnObject.datos = response.data;
+        }
+    return returnObject;
 };
-    const response = await axios.get(`https://www.omdbapi.com/?s=${searchText}&page=${page}&apikey=${APIKEY}`);
+
+const OMDBSearchComplete = async (searchText) => {
+    let returnObject = {
+    respuesta : false,
+    datos : []
+    };
+    const response = await  OMDBSearchByPage(searchText) 
     if (response.data.Response === "True") {
         returnObject.respuesta = true;
         returnObject.datos = response.data;
     }
-return returnObject;
+    return returnObject;
 };
-const OMDBSearchComplete = async (searchText) => {
-let returnObject = {
-respuesta : false,
-datos : []
-};
-const response = await axios.get(`https://www.omdbapi.com/?s=${searchText}&apikey=${APIKEY}`);
-if (response.data.Response === "True") {
-    returnObject.respuesta = true;
-    returnObject.datos = response.data;
-}
-return returnObject;
-};
+
 const OMDBGetByImdbID = async (imdbID) => {
-let returnObject = {
-respuesta : false,
-datos : {}
-};
-const response = await axios.get(`https://www.omdbapi.com/?i=${imdbID}&apikey=${APIKEY}`);
-if (response.data.Response === "True") {
-    returnObject.respuesta = true;
-    returnObject.datos = response.data;
-}
-return returnObject;
+    let returnObject = {
+    respuesta : false,
+    datos : {}
+    };
+    const response = await axios.get(`http://www.omdbapi.com/?i=${imdbID}&apikey=${APIKEY}`);
+    if (response.data.Response === "True") {
+        returnObject.respuesta = true;
+        returnObject.datos = response.data;
+    }
+    return returnObject;
 };
 export {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID};
